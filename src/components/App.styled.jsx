d9cbac660b0e1b3672a3807232d11013d0b9ed75
YuliiaPro <129890065+YuliiaPro@users.noreply.github.com>
2023-10-21 00:45:29 +0300

    add center position

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,6 +1,10 @@
 import styled from '@emotion/styled'
 
 export const Container = styled.div`
+ position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 padding: 20px;
 color: #2e2f42;
 background-color: #ffffffff;
